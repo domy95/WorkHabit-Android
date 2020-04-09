24352f012c5058929a27198fa8c65293f48d88fb
Artur Domínguez <domy95.adg@gmail.com>
2020-04-09 18:01:29 +0200

    [ADDED] Needed util methods

--- app/src/main/java/cat/coronout/workhabit/util/Utils.java
+++ app/src/main/java/cat/coronout/workhabit/util/Utils.java
@@ -1,7 +1,9 @@
 package cat.coronout.workhabit.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public abstract class Utils {
 
@@ -9,6 +11,32 @@ public abstract class Utils {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return calendar;
+    }
+
+    public static Date getDateFromCalendar(Calendar calendar) {
+        return calendar.getTime();
+    }
+
+    public static String getUserDateFormat(Date date) {
+        if (date == null)
+            return "";
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        return format.format(date);
+    }
+
+    public static String getLocalStorageDateFormat(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        return format.format(date);
+    }
+
+    public static String getUserHour(int hour, int minute) {
+        String finalHour = "";
+        if (hour < 10) finalHour += ("0" + hour);
+        else finalHour += hour;
+        finalHour += ":";
+        if (minute < 10) finalHour += ("0" + minute);
+        else finalHour += minute;
+        return finalHour;
     }
 
 }
